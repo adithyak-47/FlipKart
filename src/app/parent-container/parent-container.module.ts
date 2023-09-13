1ecb9b89e64ae83e56d592a2a1741de5b40f0402
Kamla Jain <kamlajain02@gmail.com>
2023-09-13 23:08:24 +0530

    trial commit for category-list

--- src/app/parent-container/parent-container.module.ts
+++ src/app/parent-container/parent-container.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ParentContainerComponent } from './parent-container.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { CategoryListComponent } from './top-bar/category-list/category-list.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 
 
 @NgModule({
   declarations: [
     ParentContainerComponent,
-    TopBarComponent
+    TopBarComponent,
+    ProductListComponent,
+    CategoryListComponent
   ],
   imports: [
     CommonModule
---
+++ src/app/parent-container/top-bar/category-list/ICategoryList.ts
@@ -0,0 +1,5 @@
+export interface ICategoryList{
+    categoryId: number,
+    categoryThumbnailURL: string,
+    categoryName: string
+}
