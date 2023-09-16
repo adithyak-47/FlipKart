1dd18c189726cfcbef8f4fce3a5efdea0b3f9b71
Adithyakrishnan <adithyakrishnannanda@gmail.com>
2023-09-16 17:27:38 +0530

    Modified app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    
+    loadChildren: () => import('./parent-container/parent-container.module').then(m => m.ParentContainerModule)
   }
 ];
 
